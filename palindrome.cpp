6e71155b0c2cff7cd2dc5c12891ba0ea9610aa68
INIYAVAN S <iniyavansoundar2002@gmail.com>
2025-02-13 18:12:47 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,30 @@
-#include <>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int countDigits(int num) {
+    return log10(num) + 1;
+}
+bool isArmstrong(int num) {
+    int originalNum = num;
+    int sum = 0;
+    int numberOfDigits = countDigits(num);
+    while (num != 0) {
+        int digit = num % 10;
+        sum += pow(digit, numberOfDigits);
+        num /= 10;
+    }
+    return (sum == originalNum);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isArmstrong(num)) {
+        cout << num << " is an Armstrong number." << endl;
+    } else {
+        cout << num << " is not an Armstrong number." << endl;
+    }
+    return 0;
+}
